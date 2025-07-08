a00ed15c2b85d480cf4f022e87da915fd11f5ca6
George Nicolaou <orionas.elite@gmail.com>
2025-07-08 19:03:55 +0300

    feat: show debug logs in UI and bump version

--- includes/GnCustomPostSelector.php
+++ includes/GnCustomPostSelector.php
@@ -27,7 +27,7 @@ class GNWEBDEVCY_GnCustomPostSelector extends DiviExtension {
 	 *
 	 * @var string
 	 */
-       public $version = '1.0.5';
+       public $version = '1.0.6';
 
 	/**
 	 * GNWEBDEVCY_GnCustomPostSelector constructor.
